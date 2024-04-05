97d17b11d710b9fb8e5e6174c55d66d3d308bc57
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-04-05 16:44:19 -0500

    debugging database/server connection issues

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://localhost:27017/social-network-api');
 
-module.exports = connection; 
+module.exports = mongoose.connection; 
